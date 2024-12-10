b9662c3bde1cc827e5b55b7d435cc78450747448
Rafael93150 <tavaresrafael93@gmail.com>
2024-12-10 16:29:20 +0100

    update user model

--- back/models/user.js
+++ back/models/user.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
 	{
-		name: {
+		firstname: {
+			type: String,
+			required: true,
+		},
+		lastname: {
 			type: String,
 			required: true,
 		},
@@ -14,6 +18,19 @@ const userSchema = new mongoose.Schema(
 		password: {
 			type: String,
 			required: true,
+		},
+		role: {
+			type: String,
+			default: "student",
+			enum: ["student", "teacher", "admin"],
+		},
+		classId: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Class",
+		},
+		programId: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Program",
 		},
 	},
 	{ timestamps: true }
