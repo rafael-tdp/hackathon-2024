b47abc09099c6c2a0e6da6d0ea9e10c2769598a9
Asma MOKEDDES <mokeddes.asma@outlook.fr>
2024-12-10 18:19:09 +0100

    feat(tailwind): adding tailwind config

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import path from "path";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
+    optimizeDeps: {
+        exclude: ["oh-vue-icons/icons"],
+    },
+    server: {
+        host: true,
+        port: 5173,
+        watch: {
+            usePolling: true,
+        },
+    },
+});
