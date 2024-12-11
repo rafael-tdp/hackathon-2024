e46cd90599f9759d04f6979b0874e720b2b5c08b
Rafael93150 <tavaresrafael93@gmail.com>
2024-12-11 16:30:00 +0100

    add sidebar

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -3,9 +3,10 @@ import Login from '../views/Login.vue';
 import Register from '../views/Register.vue';
 import Planification from "../views/Planification.vue";
 import Classes from "../views/classes/Classes.vue";
+import Home from '../views/Home.vue';
 
 const routes = [
-  {  path: '/', redirect: '/login' }, 
+  {  path: '/', name: 'Home', component: Home },
   { path: '/login', name: 'Login', component: Login },
   { path: '/register', name: 'Register', component: Register },
   { path: "/planification", name: "Planification", component: Planification },
