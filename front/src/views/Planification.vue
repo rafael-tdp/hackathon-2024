431b1c62530341c02aba16165da802e6c12cd13e
Rafael93150 <tavaresrafael93@gmail.com>
2024-12-12 10:51:34 +0100

    remove planification comments

--- front/src/views/Planification.vue
+++ front/src/views/Planification.vue
@@ -2,14 +2,7 @@
   <LayoutAuthenticated>
     <div class="min-h-screen py-12 px-6">
 			<div class="flex justify-between items-center mb-8">
-        <!-- <h1 class="text-2xl font-bold text-gray-700">Planification Académique</h1> -->
         <PageTitle text="Planification Académique" />
-        <!-- <button
-          @click="addEvent"
-          class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
-        >
-          Ajouter un cours
-        </button> -->
         <NewItemButton
           @click="addEvent"
           text="Nouveau Cours"
