02a82c19c25a23050b3fa7c10ba4aba1164b20ed
Rafael93150 <tavaresrafael93@gmail.com>
2024-12-13 19:18:06 +0100

    add stats route

---
+++ back/routes/statsRoutes.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const ApiResponse = require("../models/apiResponse");
+const { getStats } = require("../services/statsServices");
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+	try {
+		const stats = await getStats();
+		res.json(
+			new ApiResponse({
+				success: true,
+				data: stats,
+			})
+		);
+	} catch (error) {
+		res.status(500).json({ error: error.message });
+	}
+});
+
+module.exports = router;
